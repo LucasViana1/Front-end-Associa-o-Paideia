4fc1a3fa67a0e88c45fec44dd07e24c0b0b52734
Lucas Viana <lucasviana112@gmail.com>
2019-07-31 19:28:50 -0300

    ajuste dominios de teste

--- config.js
+++ config.js
@@ -3,9 +3,11 @@ exports.retorno = function(){
 }
 
 exports.server = function(){
-    return 'http://localhost:3000/'
+    //return 'http://localhost:3000/'
+    return 'https://fast-reef-93216.herokuapp.com/'
 }
 
 exports.website = function(){
-    return 'http://localhost:8080/#/'
+    //return 'http://localhost:8080/#/'
+    return 'https://infinite-woodland-99670.herokuapp.com/#/'
 }
