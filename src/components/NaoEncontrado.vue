8ed08dab01d88f7df20830c70b9c12ff31870add
Lucas Viana <lucasviana112@gmail.com>
2019-08-14 16:09:23 -0300

    criacao de rota para erro 404

---
+++ src/components/NaoEncontrado.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-container>
+        <center> 
+            <br> <h1> 😨 <br><br> A página que você está procurando não existe.  </h1>
+        </center>
+    </b-container>
+</template>
+
+<style>
+h1{
+    color:rgb(24, 24, 24);
+}
+</style>
